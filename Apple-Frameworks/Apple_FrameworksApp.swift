8ff02fef0da1bbcb9c8ba139d7cd7f28011d1346
Christian Skorobogatow <christiansko@icloud.com>
2020-12-22 15:50:31 +0100

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Christian Skorobogatow on 22/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
