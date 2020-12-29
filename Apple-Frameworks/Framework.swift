147b61748a1a64893e20c501d546e08b054e4c97
Christian Skorobogatow <christiansko@icloud.com>
2020-12-29 12:45:28 +0100

    Finished grid + added scrollview

--- Apple-Frameworks/Framework.swift
+++ Apple-Frameworks/Framework.swift
@@ -7,12 +7,13 @@
 
 import Foundation
 
-struct Framework {
+struct Framework: Hashable, Identifiable {
+    var id = UUID()
+    
     let name: String
     let imageName: String
     let urlString: String
     let description: String
-    
 }
 
 struct MockData {
